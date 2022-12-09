941aaaa058b24a6c16e4aa89895ba1207a9c131c
Рома Юсупов <roma_yusupov_05@mail.ru>
2022-12-09 21:49:05 +0300

    test

--- pz_8/Program.cs
+++ pz_8/Program.cs
@@ -10,6 +10,27 @@ namespace pz_8
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int count = 0;
+            int m = 4;
+            int n = 4;
+            int[,] matrix = new int[m, n];
+            Console.WriteLine("Сгенерированная матрица");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = rnd.Next(2, 8);
+                    Console.Write("{0,3} ", matrix[i, j].ToString());
+                    if ((j > i) && (matrix[i, j] >= 0))
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Количество = {0}", count);
+            Console.ReadKey();
         }
     }
 }
