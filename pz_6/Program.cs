5cda465861edb8241fb42c82f9caa33bcf49726f
Рома Юсупов <roma_yusupov_05@mail.ru>
2022-12-09 21:47:18 +0300

    wokr

--- pz_6/Program.cs
+++ pz_6/Program.cs
@@ -10,6 +10,31 @@ namespace pz_6
     {
         static void Main(string[] args)
         {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int a1 = 0;
+            int a2 = 1;
+            int n = 1;
+            int sum;
+            int res = -1;
+            while (a1 <= a)
+            {
+                sum = a1 + a2;
+                a1 = a2;
+                a2 = sum;
+                n += 1;
+                if (a1 == a)
+                {
+                    res = n;
+
+                }
+                else if (a == 0)
+                {
+                    res = 0;
+                    Console.WriteLine(res);
+                }
+                Console.WriteLine(n);
+            }
+            Console.ReadKey();
         }
     }
 }
