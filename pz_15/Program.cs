65dc2e450243c1f81e2dd5c9c1e2103052f7c4c6
Рома Юсупов <roma_yusupov_05@mail.ru>
2022-12-09 21:58:00 +0300

    text

--- pz_15/Program.cs
+++ pz_15/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,36 @@ namespace pz_15
     {
         static void Main(string[] args)
         {
+            string papka = "D:\\програмест";
+            string[] Files = Directory.GetFiles(papka);
+            string s;
+
+            if (Files.Length == 0)
+            {
+                Console.WriteLine("Файлов нет.");
+            }
+            else
+            {
+                foreach (string file in Files)
+                {
+                    if (File.Exists(file))
+                    {
+                        Console.WriteLine("Файл " + file);
+                        using (StreamReader sr = new StreamReader(file))
+                        {
+                            s = sr.ReadToEnd();
+                        }
+                        using (StreamWriter sw = new StreamWriter("file2.txt", false, Encoding.Default))
+                        {
+                            sw.WriteLineAsync(s);
+                        }
+
+
+                    }
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
