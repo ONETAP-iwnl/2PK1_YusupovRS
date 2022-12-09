e31cef23ff82a86124533915556f6084b3d65bf0
Рома Юсупов <roma_yusupov_05@mail.ru>
2022-12-09 21:56:50 +0300

    text

--- pz_12/Program.cs
+++ pz_12/Program.cs
@@ -10,6 +10,22 @@ namespace pz_12
     {
         static void Main(string[] args)
         {
+            double A = Convert.ToDouble(Console.ReadLine());
+            double B = Convert.ToDouble(Console.ReadLine());
+            double C = Convert.ToDouble(Console.ReadLine());
+            SortInc3(A, B, C);
+        }
+
+
+        static void SortInc3(double A, double B, double C)
+        {
+            double[] array = { A, B, C };
+            Array.Sort(array);
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
         }
     }
 }
