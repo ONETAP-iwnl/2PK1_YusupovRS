1b65643cdc151f35b369f10b0cc48a7a722b805c
Рома Юсупов <roma_yusupov_05@mail.ru>
2023-01-21 10:04:47 +0300

    pz_20

---
+++ pz_20/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace pz_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Registr(text);
+        }
+
+       static string Registr(string text)
+        {
+            char[] arr = text.ToCharArray();
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (Char.IsUpper(arr[i]) == true)
+                {
+                    arr[i] = char.ToLower(arr[i]);
+                }    
+                else if (Char.IsLower(arr[i]) == true)
+                {
+                    arr[i] = char.ToUpper(arr[i]);
+                }
+            }
+            string newtext = new string(arr);
+            Console.WriteLine(newtext);
+            return newtext;
+        }
+    }
+}
